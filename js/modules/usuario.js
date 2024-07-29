ce45123edfaae182c22be20ebe90f554c796664b
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-07-29 12:05:43 -0500

    docs: :memo: Se añadio la documentacion en el codigo

--- js/modules/usuario.js
+++ js/modules/usuario.js
@@ -1,25 +1,38 @@
 import { connect } from "../../helpers/db/connect.js";
 
-export class usuario extends connect {
+export class Usuario extends connect {
     static instanceUsuario;
     db;
     collection;
 
     constructor() {
-        if (usuario.instanceUsuario) {
-            return usuario.instanceUsuario;
+        if (Usuario.instanceUsuario) {
+            return Usuario.instanceUsuario;
         }
         super();
-        this.db = this.conexion.db(this.getDbName);
+        this.db = this.conexion.db(this.getDbName());
         this.collection = this.db.collection('usuario');
-        usuario.instanceUsuario = this;
+        Usuario.instanceUsuario = this;
     }
 
     destructor() {
-        usuario.instanceUsuario = undefined;
+        Usuario.instanceUsuario = undefined;
         connect.instanceConnect = undefined;
     }
 
+    /**
+     * Crea un nuevo usuario en la base de datos.
+     *
+     * @param {Object} usuarioData - Los datos del usuario a crear.
+     * @param {string} usuarioData.id - El ID del usuario.
+     * @param {string} usuarioData.nombre - El nombre del usuario.
+     * @param {string} usuarioData.rol - El rol del usuario.
+     * 
+     * @returns {Promise<Object>} Una promesa que se resuelve con un mensaje de éxito y el objeto del usuario creado sin la contraseña,
+     * o un objeto de error si ya existe un usuario con el mismo ID o si ocurre un error durante la operación.
+     *
+     * @throws {Error} Lanza un error si hay algún problema durante la conexión a la base de datos o durante la creación del usuario.
+     */
     async crearUsuario(usuarioData) {
         try {
             await this.conexion.connect();
