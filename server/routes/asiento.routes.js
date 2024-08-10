202a13bd3032b50c44d691d876f95230853f056a
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-08-10 09:35:59 -0500

    feat: :construction: Se realizo la ruta para listar asientos de una sala

---
+++ server/routes/asiento.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const asiento = require('../modules/asiento');
+const appAsiento = express.Router();
+
+appAsiento.get("/listarAsientos/:id", async (req, res) => {
+    let obj = new asiento();
+    const id = req.params.id;  
+    const asientoObj = { id: parseInt(id, 10) };
+
+    try {
+        const asientos = await obj.listarAsientos(asientoObj);
+
+        if (asientos.error) {
+            res.status(404).send(asientos);
+        } else {
+            res.status(200).send(asientos);
+        }
+    } catch (error) {
+        res.status(500).send({ error: 'Error al consultar los asientos' });
+    }
+});
+
+module.exports = appAsiento
