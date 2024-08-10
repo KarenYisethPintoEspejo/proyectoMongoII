dcb91832b06bc5fe1c4ace482492be61d8329991
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-08-10 09:34:27 -0500

    feat: :construction: Se creo el metodo para listar asientos por una sala en especifico
    El metodo busca dentro de la coleccion asiento todos los asientos de una sala en especifico y muestra error si la sala no tiene asientos registrados

---
+++ server/modules/asiento.js
@@ -0,0 +1,38 @@
+const connect  = require('../helpers/db/connect')
+
+
+
+module.exports = class asiento extends connect {
+    static instanceAsiento;
+    db;
+    collection;
+
+    constructor() {
+        if (asiento.instanceAsiento) {
+            return asiento.instanceAsiento;
+        }
+        super();
+        this.db = this.conexion.db(this.getDbName);
+        this.collection = this.db.collection('asiento');
+        asiento.instanceAsiento = this;
+    }
+
+    destructor() {
+        asiento.instanceAsiento = undefined;
+        connect.instanceConnect = undefined;
+    }
+
+    async listarAsientos({id}) {
+        await this.conexion.connect();
+        try {
+            const collection = this.db.collection('asiento');
+            const res = await collection.find({ id_sala: id }).toArray(); // No desestructures el resultado
+            return { mensaje: "Lista de asientos de la sala", data: res };
+        } catch (error) {
+            return { mensaje: "La sala no tiene asientos registrados", data: id };
+        } finally {
+            await this.conexion.close();
+        }
+    }
+    
+}
