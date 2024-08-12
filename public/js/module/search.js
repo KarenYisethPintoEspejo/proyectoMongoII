e4c124a03327ae17da9773e259cf215d1a2c8f25
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-08-12 07:21:02 -0500

    feat: :construction: Se cambio el buscador y se implemento en una vista aparte

--- public/js/module/search.js
+++ public/js/module/search.js
@@ -6,7 +6,6 @@ navItems.forEach(item => {
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:5010/pelicula/listaPeliculas')
         .then(response => {
@@ -17,16 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(movies => {
             console.log('Películas obtenidas:', movies);
- 
-            displayMovies(movies);
-            displayComingSoon(movies);
+            const container = document.getElementById('movies-container');
+            container.innerHTML = '';
+
+            const searchInput = document.getElementById('search-input');
+            searchInput.addEventListener('input', () => {
+                const query = searchInput.value.toLowerCase();
+                const filteredMovies = filterMovies(movies, query);
+                displayMovies(filteredMovies); 
+            });
         })
         .catch(error => {
             console.error('Error al obtener las películas:', error);
             document.getElementById('movies-container').innerHTML = '<p>No se pudieron cargar las películas.</p>';
         });
 });
-
 
 function filterMovies(movies, query) {
     return movies.filter(movie => {
@@ -39,10 +43,8 @@ function filterMovies(movies, query) {
 
 function displayMovies(movies) {
     const container = document.getElementById('movies-container');
-    container.innerHTML = '';
-    const today = new Date();
-    const nowPlayingMovies = movies.filter(movie => new Date(movie.fecha_estreno) < today);
-    nowPlayingMovies.forEach(movie => {
+    container.innerHTML = '';  
+    movies.forEach(movie => {
         const movieItem = document.createElement('div');
         movieItem.classList.add('movie-item');
         movieItem.dataset.id = movie.id;  
@@ -56,31 +58,6 @@ function displayMovies(movies) {
         movieItem.addEventListener('click', () => {
             window.location.href = `./views/pelicula.html?movieId=${movie.id}`;
         });
-        container.appendChild(movieItem);
-    });
-}
-
-function displayComingSoon(movies) {
-    const container = document.querySelector('.movie-carousel1');
-    container.innerHTML = ''; 
-
-    const today = new Date(); 
-    const comingSoonMovies = movies.filter(movie => new Date(movie.fecha_estreno) > today);
-
-    comingSoonMovies.forEach(movie => {
-        const movieItem = document.createElement('div');
-        movieItem.classList.add('movie-item');
-        movieItem.dataset.id = movie.id;  
-        const imageUrl = movie.imagen; 
-        movieItem.innerHTML = `
-            <img src="${imageUrl}" alt="${movie.nombre}" class="movie-image">
-            <h3>${movie.nombre}</h3>
-            <p>${movie.generos.join(', ')}</p>
-        `;
-        movieItem.addEventListener('click', () => {
-            window.location.href = `./views/pelicula.html?movieId=${movie.id}`;
-        });
-
         container.appendChild(movieItem);
     });
 }
