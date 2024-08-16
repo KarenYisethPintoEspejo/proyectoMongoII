de6ca794796b1905a48b93c0bbb752aa48b26ec5
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com>
2024-08-16 11:04:25 -0500

    feat: :sparkles: Se termino con la funcionalidad y diseño del frontend de la tercera vista

--- public/js/module/asiento.js
+++ public/js/module/asiento.js
@@ -1,19 +1,39 @@
 function selectDay(element) {
+    const isActive = element.classList.contains('active');
     document.querySelectorAll('.days .day').forEach(function(day) {
         day.classList.remove('active', 'active-state');
     });
-    element.classList.add('active', 'active-state');
+    if (!isActive) {
+        element.classList.add('active', 'active-state');
+    }
 }
 
 function selectHour(element) {
+
+    const isActive = element.classList.contains('active');
+
     document.querySelectorAll('.hour-price .hour').forEach(function(hour) {
         hour.classList.remove('active', 'active-state');
     });
-    element.classList.add('active', 'active-state');
+    
+    if (!isActive) {
+        element.classList.add('active', 'active-state');
+
+        const precioTexto = element.querySelector('p').textContent.trim();
+        const precio = parseFloat(precioTexto.replace('$', '').replace(' ·3D', '')); 
+        
+        if (isNaN(precio)) {
+            console.error('Error al extraer el precio:', precioTexto);
+            return;
+        }
+        precioTotal = precio;
+        precioElemento.textContent = `$${precioTotal.toFixed(2)}`;
+    } else {
+        precioTotal = 0; 
+        precioElemento.textContent = `$${precioTotal.toFixed(2)}`;
+    }
 }
-
 const asientos = document.querySelectorAll('.asientos__lista button, .asientos__preferenciales button');
-
 asientos.forEach(asiento => {
     asiento.addEventListener('click', () => {
         asiento.classList.toggle('active');
@@ -22,20 +42,3 @@ asientos.forEach(asiento => {
 
 const precioElemento = document.querySelector('.precio h2');
 let precioTotal = 0;
-function selectHour(element) {
-    const precioTexto = element.querySelector('p').textContent.trim();
-    const precio = parseFloat(precioTexto.replace('$', '').replace(' ·3D', '')); 
-    
-    if (isNaN(precio)) {
-        console.error('Error al extraer el precio:', precioTexto);
-        return;
-    }
-    if (element.classList.contains('active')) {
-        element.classList.remove('active', 'active-state');
-        precioTotal -= precio;
-    } else {
-        element.classList.add('active', 'active-state');
-        precioTotal += precio;
-    }
-    precioElemento.textContent = `$${precioTotal.toFixed(2)}`;
-}
