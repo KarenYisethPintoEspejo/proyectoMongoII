5084379429e1ea3c92a2569873eacc8f3ea89f08
KarenYisethPintoEspejo <156373335+KarenYisethPintoEspejo@users.noreply.github.com.>
2024-08-19 22:56:09 -0500

    feat: :construction: Se implemento el temporizador para la compra y se inicio con el html de la quinta vista

---
+++ public/js/module/pago.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const countdownElement = document.getElementById('countdown');
+    let timeLeft =  14 * 60 + 59;
+    function updateCountdown() {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        
+        countdownElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        
+        if (timeLeft === 0) {
+            clearInterval(countdownInterval);
+            window.history.back();
+        } else {
+            timeLeft--;
+        }
+    }
+    const countdownInterval = setInterval(updateCountdown, 1000);
+    updateCountdown();
+});
